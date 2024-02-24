551cb93804693bef3aa5a31eeba32944e7382ac0
Mark Pashmfouroush <mark@markpash.me>
2024-02-24 20:27:15 +0000

    psiphon: fix dns resolution bug
    Could not properly use socks5, only socks5h would work. Fixed in this commit.
    
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- psiphon/p.go
+++ psiphon/p.go
@@ -5,13 +5,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon"
-	"github.com/refraction-networking/conjure/pkg/station/log"
 	"net"
 	"path/filepath"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon"
+	"github.com/refraction-networking/conjure/pkg/station/log"
 )
 
 // Parameters provide an easier way to modify the tunnel config at runtime.
@@ -150,7 +151,7 @@ func StartTunnel(
 	if len(paramsDelta) > 0 {
 		err = config.SetParameters("", false, paramsDelta)
 		if err != nil {
-			return nil, errors.New(fmt.Sprintf("SetParameters failed for delta: %v", paramsDelta))
+			return nil, fmt.Errorf("set parameters failed for delta %v : %w", paramsDelta, err)
 		}
 	}
 
@@ -337,7 +338,8 @@ func RunPsiphon(wgBind, localSocksPort, country string, ctx context.Context) err
 		"RemoteServerListSignaturePublicKey":"MIICIDANBgkqhkiG9w0BAQEFAAOCAg0AMIICCAKCAgEAt7Ls+/39r+T6zNW7GiVpJfzq/xvL9SBH5rIFnk0RXYEYavax3WS6HOD35eTAqn8AniOwiH+DOkvgSKF2caqk/y1dfq47Pdymtwzp9ikpB1C5OfAysXzBiwVJlCdajBKvBZDerV1cMvRzCKvKwRmvDmHgphQQ7WfXIGbRbmmk6opMBh3roE42KcotLFtqp0RRwLtcBRNtCdsrVsjiI1Lqz/lH+T61sGjSjQ3CHMuZYSQJZo/KrvzgQXpkaCTdbObxHqb6/+i1qaVOfEsvjoiyzTxJADvSytVtcTjijhPEV6XskJVHE1Zgl+7rATr/pDQkw6DPCNBS1+Y6fy7GstZALQXwEDN/qhQI9kWkHijT8ns+i1vGg00Mk/6J75arLhqcodWsdeG/M/moWgqQAnlZAGVtJI1OgeF5fsPpXu4kctOfuZlGjVZXQNW34aOzm8r8S0eVZitPlbhcPiR4gT/aSMz/wd8lZlzZYsje/Jr8u/YtlwjjreZrGRmG8KMOzukV3lLmMppXFMvl4bxv6YFEmIuTsOhbLTwFgh7KYNjodLj/LsqRVfwz31PgWQFTEPICV7GCvgVlPRxnofqKSjgTWI4mxDhBpVcATvaoBl1L/6WLbFvBsoAUBItWwctO2xalKxF5szhGm8lccoc5MZr8kfE0uxMgsxz4er68iCID+rsCAQM=",
 		"RemoteServerListUrl":"https://s3.amazonaws.com//psiphon/web/mjr4-p23r-puwl/server_list_compressed",
 		"SponsorId":"FFFFFFFFFFFFFFFF",
-		"UseIndistinguishableTLS":true
+		"UseIndistinguishableTLS":true,
+		"AllowDefaultDNSResolverWithBindToDevice":true
 	}`
 
 	dir := "."
